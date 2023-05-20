5f7c251eebb9475bc17f4b3e129a2e02e6032c0b
DEO-DECODE <106802468+DEO-DECODE@users.noreply.github.com>
2023-05-20 22:27:25 +0530

    Create  Remove Duplicates from Sorted Array(LeetCode).cpp

---
+++ Remove Duplicates from Sorted Array(LeetCode).cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int i=0, j;
+    for(int j=1; j<nums.size(); ++j){
+        if(nums[i]!=nums[j]){
+            // Present wala number, next wale k 
+            // equal nhi hai
+            i++;
+            nums[i]=nums[j];
+        }
+        // agar equal hai to hm us case mein i
+        // i ko increment nhi kara rhein.
+    }
+    return i+1;
+    }
+};
