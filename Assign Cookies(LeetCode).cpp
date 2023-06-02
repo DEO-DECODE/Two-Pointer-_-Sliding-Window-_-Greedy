078e2a46fa98a0769405bf6c5a8f6057cd7fc3fd
DEO-DECODE <106802468+DEO-DECODE@users.noreply.github.com>
2023-06-02 15:18:09 +0530

    Create Assign Cookies(LeetCode).cpp

---
+++ Assign Cookies(LeetCode).cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(s.begin(), s.end());
+    sort(g.begin(), g.end());
+    int cnt = 0;
+    for (int i = 0; i < g.size(); ++i)
+    {
+        auto low1 = lower_bound(s.begin(), s.end(), g[i]) - s.begin();
+        if (low1 < s.size())
+        {
+            cnt++;
+            s.erase(find(s.begin(), s.end(), s[low1]));
+        }
+    }
+    return cnt;
+    }
+};
